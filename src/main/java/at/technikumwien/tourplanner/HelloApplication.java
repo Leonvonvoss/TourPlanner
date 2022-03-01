ec4457fe9513849caab1df7c83d51c3625f73363
Leon von Voss <if20b169@technikum-wien.at>
2022-03-01 19:55:00 +0100

    Added basics
    Added basics like 'Specification.pdf', basic structure, first draft of hello view, ...

--- src/main/java/at/technikumwien/tourplanner/HelloApplication.java
+++ src/main/java/at/technikumwien/tourplanner/HelloApplication.java
@@ -11,9 +11,9 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Tour-Planner!");
         stage.setScene(scene);
         stage.show();
     }
