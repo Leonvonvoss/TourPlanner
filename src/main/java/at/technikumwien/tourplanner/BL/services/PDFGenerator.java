793e78282503269a6575c4751321c97901307ea9
Leon von Voss <if20b169@technikum-wien.at>
2022-04-19 12:42:30 +0200

    Edited PDFGenerator for img

--- src/main/java/at/technikumwien/tourplanner/BL/services/PDFGenerator.java
+++ src/main/java/at/technikumwien/tourplanner/BL/services/PDFGenerator.java
@@ -1,17 +1,22 @@
 package at.technikumwien.tourplanner.BL.services;
 
+import at.technikumwien.tourplanner.TourPlannerApplication;
 import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.colors.ColorConstants;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.element.Text;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 
 public class PDFGenerator {
 
@@ -24,13 +29,22 @@ public class PDFGenerator {
                 .setFontSize(14)
                 .setBold()
                 .setFontColor(ColorConstants.RED);
-        document.add(loremIpsumHeader);
-        document.add(new Paragraph("LOREM_IPSUM_TEXT"));
         Paragraph paragraph = new Paragraph();
         Table table = new Table(3).useAllAvailableWidth();
         for(int i = 0; i <= 5; i++) {
             table.addCell("Hallo");
         }
+        Paragraph imageHeader = new Paragraph("Lorem Ipsum Image ...")
+                .setFont(PdfFontFactory.createFont(StandardFonts.TIMES_ROMAN))
+                .setFontSize(18)
+                .setBold()
+                .setFontColor(ColorConstants.GREEN);
+        document.add(imageHeader);
+        URL resource = TourPlannerApplication.class.getResource("img/menu_close.png");
+        ImageData imageData = ImageDataFactory.create(resource);
+        document.add(new Image(imageData));
+        document.add(loremIpsumHeader);
+        document.add(new Paragraph("LOREM_IPSUM_TEXT"));
         paragraph.add(table);
         document.add(paragraph);
         document.close();
